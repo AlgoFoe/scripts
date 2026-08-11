d342e3376b7679989dd38255400f5518a6ea0da6
AlgoFoe <varun.singh10011@gmail.com>
2026-08-11 17:44:38 +0530

    update the pr title

--- bash/migrate_to_uv.sh
+++ bash/migrate_to_uv.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 ORG="brainglobe"
 BRANCH="migrate/tox-to-uv-action"
-PR_TITLE="ci: migrate from tox to pure-uv test action"
+PR_TITLE="ci: migrate from tox to uv test action"
 PR_BODY='Migrate CI from `neuroinformatics-unit/actions/test@v2` (tox) to `neuroinformatics-unit/actions/test-uv@main` (pure uv).'
 
 WORK_DIR="$(pwd)/.uv_migration_work"
